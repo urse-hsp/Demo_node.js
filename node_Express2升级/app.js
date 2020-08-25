1eed0475f70f542bc9920ef53234dcc3efb7d389
侯少平 <hsp_email@163.com>
2020-08-25 17:35:59 +0800

    完成接口操作后台，无敌啊

--- node_Express2升级/app.js
+++ node_Express2升级/app.js
@@ -20,7 +20,7 @@ app.use(urlencoded({ extended: false })) // 表单请求
 
 const conn = mysql.createConnection(option)
 
-app.listen(8880, () => console.log('启动服务'))
+app.listen(8888, () => console.log('启动服务'))
 
 let login = true
 
@@ -39,34 +39,40 @@ app.get('/login', (req, res, next) => {
 app.get('/api/getlist', (req, res, next) => {
   const sqlStr = 'SELECT * FROM students'
   conn.query(sqlStr, (err, results) => {
-    // if (err) return res.json({ code: 1, msg: '资料不存在', data: {} })
+    if (err) return res.json({ code: 1, msg: '资料不存在', data: {} })
     res.json({ code: 200, msg: '获取成功', data: results })
   })
 })
 
 app.post('/api/setList', (req, res, next) => {
-  const sqlStr = "INSERT into students (id,name,sex,age,address,hou) values(4,'小王','男',12,'吉林',2)"
+  // const sqlStr = "INSERT into students (id,name,sex,age,address,hou) values(4,'小王','男',12,'吉林',2)"
+  const json = req.body
+  const sqlStr = `INSERT into students (id,name,sex,age,address,hou) values(${json.id},'${json.name}','${json.sex}',${json.age},'${json.address}',${json.hou})`
   conn.query(sqlStr, (err, results) => {
-    res.json({ code: 200, msg: '添加成功' })
+    if (err) return res.json({ code: 1, msg: '添加失败', data: req.body })
+    res.json({ code: 200, msg: '添加成功', data: {} })
   })
 })
 
-app.put('/api/alterList', (req, res, next) => {
-  const sqlStr = "UPDATE students set name = '小老王' where id=4"
+app.put('/api/alterList/:id', (req, res) => {
+  const sqlStr = `UPDATE students set name='${req.body.name}' where id=${req.params.id}`
   conn.query(sqlStr, (err, results) => {
-    res.json({ code: 200, msg: '修改成功', data: results })
+    if (err) return res.json({ code: 1, msg: '修改失败', data: req.params })
+    res.json({ code: 200, msg: '修改成功', data: {} })
   })
 })
 
-app.delete('/api/deleteList', (req, res, next) => {
-  const sqlStr = "delete from students WHERE id = 4"
+app.delete('/api/deleteList/:id', (req, res) => {
+  const sqlStr = `delete from students WHERE id = ${req.params.id}`
   conn.query(sqlStr, (err, results) => {
+    if (err) return res.json({ code: 1, msg: '删除失败', data: req.params })
     res.json({ code: 200, msg: '删除成功' })
   })
 })
 
 app.post('/test/:data', (req, res) => {
   // 后面的表单参数会放到req.query、 路径上的参数会放到req.params里、 json参数会放到req.body里，
+  // http://localhost:8888/test/123?a=b    /text是params
   return res.json({ query: req.query, data: req.params, json: req.body })
 })
 
