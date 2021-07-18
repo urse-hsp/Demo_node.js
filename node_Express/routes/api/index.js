b46ddc2cf271b658511b5aa96e8c7e60d5fd948d
侯少平 <hsp_email@163.com>
2021-07-18 13:40:35 +0800

    init

--- node_Express/routes/api/index.js
+++ node_Express/routes/api/index.js
@@ -11,9 +11,9 @@ router.get('/getlist', (req, res, next) => {
 
   const sqlStr = `SELECT * FROM students LIMIT ${basics},${json.pageSize}`
   conn.query(sqlStr, (err, results) => {
-    if (err) return res.sendResult({}, 1, '失败')
+    if (err) return res.sendResult({}, '失败', false)
     conn.query(sqlStr2, (err, results2) => {
-      res.sendResult({ list: results, total: results2.length }, 200, '获取成功')
+      res.sendResult({ list: results, total: results2.length }, '获取成功')
     })
   })
 })
@@ -25,10 +25,10 @@ router.post('/setList', (req, res, next) => {
   conn.query(sqlStr2, (err, results) => {
     if (results.length === 0) {
       conn.query(sqlStr1, (err, results) => {
-        res.sendResult(results, 200, '添加成功')
+        res.sendResult(results, '添加成功')
       })
     } else {
-      res.sendResult(results, 202, '用户名姓名重复')
+      res.sendResult(results, '用户名姓名重复', false)
     }
   })
 })
@@ -36,16 +36,16 @@ router.post('/setList', (req, res, next) => {
 router.put('/alterList/:id', (req, res) => {
   const sqlStr = `UPDATE students set name='${req.body.name}',sex='${req.body.sex}',age='${req.body.age}',location='${req.body.location}' where id=${req.params.id}`
   conn.query(sqlStr, (err, results) => {
-    if (err) return res.sendResult(req.params, 1, '修改失败')
-    res.sendResult({}, 200, '修改成功')
+    if (err) return res.sendResult(req.params, '修改失败', false)
+    res.sendResult({}, '修改成功')
   })
 })
 
 router.delete('/deleteList/:id', (req, res) => {
   const sqlStr = `delete from students WHERE id = ${req.params.id}`
   conn.query(sqlStr, (err, results) => {
-    if (err) return res.sendResult(req.params, 1, '删除失败')
-    res.sendResult({}, 200, '删除成功')
+    if (err) return res.sendResult(req.params, '删除失败', 1)
+    res.sendResult({}, '删除成功')
   })
 })
 
--- node_Express/utils/connectMysql.js
+++ node_Express/utils/connectMysql.js
@@ -3,9 +3,9 @@ const mysql = require('mysql')
 const conn = mysql.createConnection({
   host: 'localhost', // 数据库地址
   user: 'root',
-  password: '930985128',
+  password: '',
   port: '3306',
-  database: 'login', // 底层数据库
+  database: 'form', // 底层数据库
   connectTimeout: 5000, // 链接超时
   multipleStatements: false, // 是否允许一个query中包含多条sql语句
 })
